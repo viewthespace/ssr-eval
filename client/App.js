e204064d6ae93f338c67e70110777b1a5265c878
Kyle Holzinger <kylelholzinger@gmail.com>
2019-05-01 16:39:33 -0400

    [KH] [SHS] implement simple counter

--- client/App.js
+++ client/App.js
@@ -1,3 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const App = () => <div>hello, world!</div>
+export const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>hello, world!</div>
+      <p>{count}</p>
+      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+    </>
+  )
+}
